4427bc3579c26870132053b139d4c639894a938f
Ilja Afanasev <chylmanprosto@gmail.com>
2025-02-02 19:24:36 +0300

    refactor: Logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import IconBehance from "../../assets/image/icons/behance.svg"
 export const Logo = () => {
     return (
         <a href="">
-            <IconBehance width={"30px"}/>
+            <IconBehance/>
         </a>
     );
 };
