0acae907f161338e7245d78b995be8ee1287e324
Ilja Afanasev <chylmanprosto@gmail.com>
2025-02-04 23:53:59 +0300

    feat: add header styles

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {theme} from "../../styles/Theme";
 
 
 export const Menu = (props: { menuItems: Array<string> }) => {
@@ -6,9 +7,17 @@ export const Menu = (props: { menuItems: Array<string> }) => {
         <StyledMenu>
             <ul>
                 {props.menuItems.map((item, index) => {
-                    return <li key={index}>
-                        <a href="#">{item}</a>
-                    </li>
+                    return <ListItem key={index}>
+                        <Link href="#">
+                            {item}
+                            <Mask>
+                                <span>{item}</span>
+                            </Mask>
+                            <Mask>
+                                <span>{item}</span>
+                            </Mask>
+                        </Link>
+                    </ListItem>
                 })}
             </ul>
         </StyledMenu>
@@ -19,5 +28,76 @@ const StyledMenu = styled.nav`
   ul {
     display: flex;
     gap: 30px;
+  }
+`
+
+const Link = styled.a`
+  font-family: "DM Sans", serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 130%;
+  text-align: center;
+
+  color: transparent;
+`
+
+const Mask = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 50%;
+  overflow-y: hidden;
+
+  span {
+    color: ${theme.colors.textSecond};
+  }
+
+  & + & {
+    top: 50%;
+
+    span {
+      display: inline-block;
+      transform: translateY(-50%);
+    }
+  }
+`
+
+const ListItem = styled.li`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    height: 2px;
+    background-color: ${theme.colors.textSecond};
+    transform: scale(0);
+
+    position: absolute;
+    top: 50%;
+    left: -5px;
+    right: -10px;
+    z-index: 1;
+  }
+
+  &:hover {
+    &::before {
+      transform: scale(1);
+    }
+
+    ${Mask} {
+      transform: skewX(12deg) translateX(5px);
+    }
+
+    ${Mask} span {
+      background-image: linear-gradient(to right, ${theme.colors.accent}, ${theme.colors.accentSecond});
+      color: transparent;
+      background-clip: text;
+    }
+
+    & + ${Mask} {
+      transform: skewX(12deg) translateX(-5px);
+    }
   }
 `
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,6 +3,7 @@ import {Menu} from "../../components/menu/Menu";
 import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contacts"]
 
@@ -20,4 +21,11 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
+  z-index: 9999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 41px 0 21px;
+  background-color: ${theme.colors.primaryBg};
 `
