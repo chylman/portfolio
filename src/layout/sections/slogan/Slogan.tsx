ff279fec99c8abcced18002a5c37dd819678cd50
Ilja <chylmanprosto@gmail.com>
2025-02-06 17:14:39 +0300

    feat: styled slogan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,19 +1,38 @@
 import styled from "styled-components";
-import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
+import {GradientText} from "../../../components/GradientText.tsx";
+import {theme} from "../../../styles/Theme.tsx";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
             <Container>
-                <span>For any questions please mail me:</span>
-                <Button as={"a"} href={"#"}>chylmanprosto@gmail.com</Button>
+                <FlexWrapper direction={"column"} align={"center"} justify={"center"}>
+                    <Caption>For any questions please mail me:</Caption>
+                    <Link href={"mailto:chylmanprosto@gmail.com"}>
+                        <GradientText>chylmanprosto@gmail.com</GradientText>
+                    </Link>
+                </FlexWrapper>
             </Container>
         </StyledSlogan>
     );
 };
 
 const StyledSlogan = styled.section`
-  min-height: 30vh;
-  background-color: #307c3d;
+  padding: 178px 0;
+`
+
+const Link = styled.a`
+  font-weight: 700;
+  font-size: 58px;
+  letter-spacing: -0.02em;
+  cursor: pointer;
+`
+
+const Caption = styled.span`
+  font-weight: 700;
+  font-size: 58px;
+  letter-spacing: -0.02em;
+  color: ${theme.colors.textSmall};
 `
