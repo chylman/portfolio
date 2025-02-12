0f873b5f7f787c0bc9b9767bcf284a760ceadbfd
Ilja Afanasev <chylmanprosto@gmail.com>
2025-02-12 20:41:07 +0300

    refactor: Contact

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,71 +1,21 @@
-import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
-import {theme} from "../../../styles/Theme";
-import {font} from "../../../styles/Common.ts";
+import {S} from "./Contact_Styles";
 
 export const Contact = () => {
     return (
-        <StyledContacts>
+        <S.Contacts>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <StyledForm action={"#"}>
-                    <Field placeholder={"name"}></Field>
-                    <Field placeholder={"subject"}></Field>
-                    <Field placeholder={"message"} as="textarea"></Field>
+                <S.Form action={"#"}>
+                    <S.Field placeholder={"name"}></S.Field>
+                    <S.Field placeholder={"subject"}></S.Field>
+                    <S.Field placeholder={"message"} as="textarea"></S.Field>
                     <Button type={"submit"}>Send message</Button>
-                </StyledForm>
+                </S.Form>
             </Container>
-        </StyledContacts>
+        </S.Contacts>
     );
 };
 
-const StyledContacts = styled.section`
-  padding: 100px 0;
-
-  @media ${theme.media.tablet} {
-    padding: 50px 0;
-  }
-  @media ${theme.media.mobile} {
-    padding: 20px 0;
-  }
-`
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  margin: 0 auto;
-  max-width: 540px;
-  width: 100%;
-
-  textarea {
-    resize: none;
-    height: 155px;
-  }
-
-  @media ${theme.media.mobile} {
-    max-width: initial;
-  }
-`
-const Field = styled.input`
-  padding: 7px 15px;
-  width: 100%;
-  background-color: ${theme.colors.secondBg};
-  border-radius: 5px;
-  border: 1px solid ${theme.colors.textSecond};
-
-  ${font({family: "\"Poppins\", sans-serif", weight: 300, Fmax: 18, Fmin: 16, color: theme.colors.textSecond})};
-
-  &::placeholder {
-    text-transform: capitalize;
-    color: ${theme.colors.textSecond};
-
-    ${font({family: "\"Poppins\", sans-serif", weight: 300, Fmax: 18, Fmin: 16, color: theme.colors.textSecond})};
-  }
-
-  &:focus {
-    outline: 1px solid ${theme.colors.outline};
-  }
-`
---
+++ src/layout/sections/contact/Contact_Styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
+
+const Contacts = styled.section`
+  padding: 100px 0;
+
+  @media ${theme.media.tablet} {
+    padding: 50px 0;
+  }
+  @media ${theme.media.mobile} {
+    padding: 20px 0;
+  }
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  margin: 0 auto;
+  max-width: 540px;
+  width: 100%;
+
+  textarea {
+    resize: none;
+    height: 155px;
+  }
+
+  @media ${theme.media.mobile} {
+    max-width: initial;
+  }
+`
+const Field = styled.input`
+  padding: 7px 15px;
+  width: 100%;
+  background-color: ${theme.colors.secondBg};
+  border-radius: 5px;
+  border: 1px solid ${theme.colors.textSecond};
+
+  ${font({family: "\"Poppins\", sans-serif", weight: 300, Fmax: 18, Fmin: 16, color: theme.colors.textSecond})};
+
+  &::placeholder {
+    text-transform: capitalize;
+    color: ${theme.colors.textSecond};
+
+    ${font({family: "\"Poppins\", sans-serif", weight: 300, Fmax: 18, Fmin: 16, color: theme.colors.textSecond})};
+  }
+
+  &:focus {
+    outline: 1px solid ${theme.colors.outline};
+  }
+`
+
+export const S = {
+    Contacts,
+    Form,
+    Field
+}
