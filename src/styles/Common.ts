5356093e21285a19732e004f8faa875f8ba50d0f
Ilja Afanasev <chylmanprosto@gmail.com>
2025-02-19 13:55:51 +0300

    fix: set min and max fonts

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -14,5 +14,5 @@ export const font = ({family, weight, color, lineHeight, Fmin, Fmax}: FontPropsT
     font-weight: ${weight || 400};
     color: ${color || theme.colors.textMain};
     line-height: ${lineHeight || 1.2};
-    font-size: calc( (100vw - 360px)/(1920 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px);
+    font-size: clamp(${Fmin}px, calc( (100vw - 360px)/(1920 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px), ${Fmax}px);
 `
